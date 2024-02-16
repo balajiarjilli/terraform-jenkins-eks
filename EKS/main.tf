cfc160e5698b2bd2d983bd33f6f80ed2f4155abe
balaji <arjillibalaji@gmail.com>
2024-02-16 11:17:25 +0530

    adding files to repo

---
+++ EKS/main.tf
@@ -0,0 +1,65 @@
+#VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnets
+  public_subnets = var.public_subnets
+  enable_dns_hostnames = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"=1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"=1
+  }
+}
+
+
+
+#EKS module
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.24"
+
+  cluster_endpoint_public_access  = true
+
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
+
+
+  eks_managed_node_groups = {
+    nodes = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t2.small"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+ 
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
